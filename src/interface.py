2c90c91df73640b77f747dafe88ef72daa6d9e3b
Luiz Gustavo C. Setten <luizgustavosetten@hotmail.com>
2020-09-04 07:53:56 -0300

    feat: UI with links

--- src/interface.py
+++ src/interface.py
@@ -1,6 +1,10 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 from controller import Controller
+import webbrowser
+
+def callback(url):
+    webbrowser.open_new(url)
 
 class Application:
     def __init__(self, master=None):
@@ -48,13 +52,6 @@ class Application:
         self.chaveBusca["font"] = self.fontePadrao
         self.chaveBusca.pack(side=LEFT)
 
-        # self.buscar = Button(self.quartoContainer)
-        # self.buscar["text"] = "Buscar"
-        # self.buscar["font"] = ("Calibri", "8")
-        # self.buscar["width"] = 12
-        # self.buscar["command"] = self.buscador
-        # self.buscar.pack()
-
         self.buscar = Button(self.quartoContainer)
         self.buscar["text"] = "Buscar"
         self.buscar["font"] = ("Calibri", "8")
@@ -63,6 +60,21 @@ class Application:
 
         self.mensagem = Label(self.quartoContainer, text="", font=self.fontePadrao)
         self.mensagem.pack()
+
+        self.link1 = Label(root, text="", fg="blue", cursor="hand2")
+        self.link1.pack()
+
+        self.link2 = Label(root, text="", fg="blue", cursor="hand2")
+        self.link2.pack()
+
+        self.link3 = Label(root, text="", fg="blue", cursor="hand2")
+        self.link3.pack()
+
+        self.link4 = Label(root, text="", fg="blue", cursor="hand2")
+        self.link4.pack()
+
+        self.link5 = Label(root, text="", fg="blue", cursor="hand2")
+        self.link5.pack()
 
     def inserirDB(self):
         filename = askopenfilename()
@@ -76,13 +88,42 @@ class Application:
     def buscador(self):
         chaveBusca = self.chaveBusca.get()
         database = self.databaseLabel["text"]
+
+        self.link1["text"] = "" 
+        self.link2["text"] = ""
+        self.link3["text"] = ""
+        self.link4["text"] = ""
+        self.link5["text"] = ""
+
         if chaveBusca == "" or database == "":
             self.mensagem["text"] = "A chave de busca e/ou a base de dados não pode ser nulas"
+
         else:
-            print("Implementar chamada de busca -> Linha 71:", chaveBusca, database)
-            #self.master.destroy() #Aqui apaga a janela e talvez encerre a execução
+            self.mensagem["text"] = ""
             resultados = self.controller.buscar(chaveBusca)
-            print(resultados)
+
+            try:
+                for i in range(5):
+                    resultados[i] = resultados[i].replace("\n", "")
+            except:
+                if(resultados[0] == 0):
+                    self.mensagem["text"] = "Nenhum site encontrado"
+
+            # link1 = Label(root, text="1°: " + resultados[0], fg="blue", cursor="hand2")
+            self.link1["text"] = "1°: " + resultados[0]
+            self.link1.bind("<Button-1>", lambda e: callback(resultados[0]))
+
+            self.link2["text"] = "2°: " + resultados[1]
+            self.link2.bind("<Button-1>", lambda e: callback(resultados[1]))
+
+            self.link3["text"] = "3°: " + resultados[2]
+            self.link3.bind("<Button-1>", lambda e: callback(resultados[2]))
+
+            self.link4["text"] = "4°: " + resultados[3]
+            self.link4.bind("<Button-1>", lambda e: callback(resultados[3]))
+
+            self.link5["text"] = "5°: " + resultados[4] + "\n"
+            self.link5.bind("<Button-1>", lambda e: callback(resultados[4]))
 
 root = Tk(className="Buscador")
 Application(master=root)
